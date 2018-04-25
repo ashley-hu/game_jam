67564e43fa4fc3c96e9b604a3950863e2472c1f6
ahu <hu.ashley@yahoo.com>
2018-04-25 18:22:31 -0400

    fixed boundaries for player movement

--- 2018.4-game-jam/Assets/Scripts/PlayerMovement.cs
+++ 2018.4-game-jam/Assets/Scripts/PlayerMovement.cs
@@ -2,17 +2,41 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+/*
+ * PlayerMovement class
+ * This moves the player with the mouse
+ * */
+
 public class PlayerMovement : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+	private float playerX;
+	private float playerY;
 
 	// Update is called once per frame
 	void Update () {
 		Vector3 mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 1);
 		Vector3 currMousePos = Camera.main.ScreenToWorldPoint(mousePos);
-		transform.position = currMousePos;
+
+		if (currMousePos.x > -7 && currMousePos.x < 7 && currMousePos.y < 3.5f && currMousePos.y > -3.5f) {
+			transform.position = currMousePos;
+		} else { //set the bounds so player can not move out of camera
+			playerX = currMousePos.x;
+			playerY = currMousePos.y;
+
+			if (currMousePos.x < -7) {
+				playerX = -7;
+			} else if (currMousePos.x > 7) {
+				playerX = 7;
+			} 
+
+			if (currMousePos.y < -3.5f) {
+				playerY = -3.5f;
+			} else if (currMousePos.y > 3.5f) {
+				playerY = 3.5f;
+			}
+
+			transform.position = new Vector3 (playerX, playerY, currMousePos.z);
+		}
 	}
 }
