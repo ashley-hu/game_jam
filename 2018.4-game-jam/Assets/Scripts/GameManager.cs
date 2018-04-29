b5bc3750a6aa452f9674685adca640baef3c6633
ahu <hu.ashley@yahoo.com>
2018-04-29 12:40:04 -0400

    added decrease rage meter and destory all objects when max rage is reached

--- 2018.4-game-jam/Assets/Scripts/GameManager.cs
+++ 2018.4-game-jam/Assets/Scripts/GameManager.cs
@@ -12,12 +12,16 @@ public class GameManager : MonoBehaviour {
 	private GameObject scoreText;
 	private float score;
 	private float roundScore;
+	private float maxRage;
+	private GameObject[] allObstacles;
 
 	// Use this for initialization
 	void Start () {
 		scoreText = GameObject.FindWithTag ("Score");
 		score = 0;
+		maxRage = 1;
 		rageMeter.value = 0;
+		rageMeter.maxValue = maxRage;
 		rageReceived = 0;
 	}
 	
@@ -35,6 +39,25 @@ public class GameManager : MonoBehaviour {
 		} else {
 			//change the color from yellow to red as you receive more "rage"
 			fillMeter.color = Color.Lerp (Color.yellow, Color.red, rageReceived / 1);
+		}
+			
+		if (rageMeter.value == maxRage) {
+			UnleashRage ();
+		} else if (rageMeter.value > maxRage) {
+		}
+	}
+
+	//Destroy all objects in scene
+	public void UnleashRage(){
+		allObstacles = GameObject.FindGameObjectsWithTag("Obstacle");
+		if (Input.GetKeyDown ("space")) {
+			for(var i = 0 ; i < allObstacles.Length ; i ++){
+				Destroy(allObstacles[i]);
+			}
+			rageReceived = 0;
+			maxRage -= 0.1f;
+			rageMeter.maxValue = maxRage;
+			score -= 100;
 		}
 	}
 }
