64f0bcc2d9c5dbf8682a0a1ef2056704d16420f2
ahu <hu.ashley@yahoo.com>
2018-04-28 23:00:14 -0400

    Added code for instantiating prefabs and having it fall down the screen

---
+++ 2018.4-game-jam/Assets/Scripts/BlockOptions.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockOptions : MonoBehaviour {
+
+	private float speed;
+
+	// Use this for initialization
+	void Start () {
+		speed = Random.Range (0f, 1f);
+		transform.position = new Vector3 (transform.position.x, transform.position.y + speed, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//move the prefab down the screen
+		transform.position -= new Vector3(0, 1+ speed, 0) * Time.deltaTime * 5;
+
+		//prefab will be destroyed
+		if (transform.position.y < -5.5f) {
+			Destroy (this.gameObject);
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D coll){
+	}
+}
--- 2018.4-game-jam/Assets/Scripts/FallingDown.cs
+++ 2018.4-game-jam/Assets/Scripts/FallingDown.cs
@@ -4,13 +4,31 @@ using UnityEngine;
 
 public class FallingDown : MonoBehaviour {
 
+	public Transform blocks;
+	private float posX;
+	private float posY;
+	private float currTimer;
+
 	// Use this for initialization
 	void Start () {
-		
+		currTimer = 1f;
+		for(int x = 0; x < 5; x++){
+			posX = Random.Range (-6.5f, 6.5f);
+			posY = Random.Range (7.0f, 10.0f);
+			Instantiate(blocks, new Vector3(posX, posY, 0), Quaternion.identity);
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		currTimer -= Time.deltaTime;
+		if (currTimer < 0) {
+			currTimer = 1f;
+			for (int x = 0; x < 5; x++) {
+				posX = Random.Range (-6.5f, 6.5f);
+				posY = Random.Range (7.0f, 10.0f);
+				Instantiate (blocks, new Vector3 (posX, posY, 0), Quaternion.identity);
+			}
+		}
 	}
 }
