e951b70ea7aca5ca464deeb01a844460ccb74168
ahu <hu.ashley@yahoo.com>
2018-04-29 00:59:48 -0400

    added scoring

---
+++ 2018.4-game-jam/Assets/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+
+	private GameObject scoreText;
+	private float score;
+	private float roundScore;
+
+	// Use this for initialization
+	void Start () {
+		scoreText = GameObject.FindWithTag ("Score");
+		score = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (scoreText != null) {
+			score += Time.deltaTime;
+			roundScore = Mathf.RoundToInt (score);
+			scoreText.GetComponent<Text> ().text = "Score: " + roundScore.ToString();
+		}
+	}
+}
